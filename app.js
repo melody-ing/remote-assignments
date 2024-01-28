460ff80ae0809403e2d906bae2c59f54d5c533ad
Melody-ing <m9930716@gmail.com>
2024-01-28 22:41:37 +0800

    change html position

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const cors = require("cors");
 const app = express();
 
 app.use(cors());
-app.use(express.static("public"));
+app.use(express.static("sum.html"));
 
 app.get("/", (req, res) => {
   res.send("<h1>Hello, My Server!</h1>");
