3c85754b96198f1299d445d7531ceba5bd6ca119
Melody-ing <m9930716@gmail.com>
2024-02-07 18:03:11 +0800

    add Boxs

--- src/Section/Boxs.js
+++ src/Section/Boxs.js
@@ -1,8 +1,4 @@
-<<<<<<< HEAD
 export default function Boxs() {
-=======
-export function Boxs() {
->>>>>>> 234c389ea046719a97b0ae6514a6d1f82ad27d21
   return (
     <div className="boxs">
       <div className="box">Content Box 1</div>
